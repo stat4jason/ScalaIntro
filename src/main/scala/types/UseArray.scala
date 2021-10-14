36d892bac1b6d421aa1553fc7c54b26466e8d0ad
simon <simon@dancingcloudservices.com>
2021-10-14 15:45:59 -0600

    #2 coding practice; apply and method arguments

--- src/main/scala/types/UseArray.scala
+++ src/main/scala/types/UseArray.scala
@@ -6,13 +6,19 @@ object UseArray {
 //    val numbers = Array[Int](1, 2, 3, 4)
 //    val numbers = Array()
     // Array is a java array. Cannot extend or shrink!!!
-    val numbers = Array(1, 2, 3, 4)
+//    val numbers = Array(1, 2, 3, 4)
+    val numbers = Array.apply(1, 2, 3, 4)
     println(s"The first element of the array is ${numbers(0)}")
+    println(s"The first element of the array is ${numbers.apply(0)}")
     numbers(1) = 100
+    numbers.update(1, 200)
     var idx = 0
     while (idx < numbers.length) {
       println(s"number is ${numbers(idx)}")
       idx += 1
     }
+
+    val empty = new Array(4)
+    println(s"array has length ${empty.length}")
   }
 }
--- src/main/scala/usefunctions/FunctionStuff.scala
+++ src/main/scala/usefunctions/FunctionStuff.scala
@@ -1,6 +1,24 @@
 package usefunctions
 
 object FunctionStuff {
+  def manyArgs(day: Int, month: Int, year: Int = 2022): Unit = {
+    println(s"day is ${day}, month is ${month}, year is ${year}")
+  }
+
+  def moreArgs(s: String, ss: String *): Unit = {
+    println(s"first is ${s}")
+    val args = ss.toIterator
+    while (args.hasNext) {
+      println(s"> ${args.next()}");
+    }
+
+  }
+
+  def apply(x: Int): Int = {
+    println(s"executing FunctionStuff.apply(${x})")
+    x * 2
+  }
+
   def addOne(x: Int): Int = {
     println("Calculating.....")
     x + 1 // return keyword exists, but is rarely needed/used
@@ -14,5 +32,18 @@ object FunctionStuff {
       println(s"calculated the number ${n}")
       n
     }
+
+    println(s"number has value ${number}")
+
+//    println(s"apply(3) gives ${FunctionStuff.apply(3)}")
+    println(s"apply(3) gives ${FunctionStuff(3)}")
+
+    manyArgs(1, 2, 2022)
+    manyArgs(month = 2, day = 1, year = 2022)
+    manyArgs(month = 2, day = 1)
+
+    moreArgs("Fred", "Jim", "Sheila")
+    val names = List("Jim", "Sheila")
+    moreArgs("Fred", names:_*)
   }
 }
